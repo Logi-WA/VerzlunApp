fad083fdd5a794dd4d5bf17b425ec4e2b98528f8
Armann-Sch <armannsch@gmail.com>
2025-03-29 12:01:21 +0000

    Implemented a Product list fragment and custom adapter class

---
+++ app/src/main/java/is/hi/hbv601g/verzlunapp/adapters/BrowseAdapter.java
@@ -0,0 +1,105 @@
+package is.hi.hbv601g.verzlunapp.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import is.hi.hbv601g.verzlunapp.R;
+import is.hi.hbv601g.verzlunapp.databinding.FragmentProductListItemBinding;
+import is.hi.hbv601g.verzlunapp.persistence.Product;
+
+public class BrowseAdapter extends RecyclerView.Adapter<BrowseAdapter.MyViewHolder> {
+
+    private ArrayList<Product> product_list;
+
+    public BrowseAdapter(ArrayList<Product> products) {
+        this.product_list = products;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_product_list_item, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return product_list.size();
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Product currentProd = product_list.get(position);
+        /*holder.binding.productId.setText(Long.toString(currentProd.getId()));
+        holder.binding.productName.setText(currentProd.getName());
+        holder.binding.productPrice.setText(Double.toString(currentProd.getPrice()));
+        */
+        //holder.productIndex.setText(Integer.toString(position));
+        holder.productId.setText(Long.toString(currentProd.getId()));
+        holder.productName.setText(currentProd.getName());
+        holder.productPrice.setText(Double.toString(currentProd.getPrice()));
+        holder.addToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Add " + currentProd.getName() + " to car.");
+            }
+        });
+        holder.addToWishlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Add " + currentProd.getName() + " to wishlist.");
+            }
+        });
+        holder.productCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Go to product " + currentProd.getId() + ".");
+            }
+        });
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private FragmentProductListItemBinding binding;
+        private int productIndex;
+        private Long Id;
+        private CardView productCard;
+        private TextView productId;
+        private ImageView productImage;
+        private TextView productName;
+        private TextView productPrice;
+        private TextView productRating;
+
+        private Button addToCart;
+        private Button addToWishlist;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            productId = itemView.findViewById(R.id.product_id);
+            productImage = itemView.findViewById(R.id.product_img);
+            productName = itemView.findViewById(R.id.productName);
+            productPrice = itemView.findViewById(R.id.productPrice);
+            productRating = itemView.findViewById(R.id.productRating);
+
+            addToCart = itemView.findViewById(R.id.addToCart);
+            addToWishlist = itemView.findViewById(R.id.addToWishlist);
+            productCard = itemView.findViewById(R.id.productCard);
+        }
+
+        public int getIndex() {
+            return productIndex;
+        }
+
+        public Long getId() {
+            return Id;
+        }
+    }
+}
