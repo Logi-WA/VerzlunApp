9dc52a85dfbaf59c153398c07a72f3762e698c07
Armann-Sch <armannsch@gmail.com>
2025-04-07 14:33:53 +0000

    Defined interface for Data Access Object

---
+++ app/src/main/java/is/hi/hbv601g/verzlunapp/database/UserInfoDao.java
@@ -0,0 +1,22 @@
+package is.hi.hbv601g.verzlunapp.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import io.reactivex.Completable;
+import io.reactivex.Single;
+
+@Dao
+public interface UserInfoDao {
+    @Query("SELECT * FROM userInfo WHERE uid = 1")
+    UserInfo getUserInfo();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertUserInfo(UserInfo ui);
+
+    @Delete()
+    void deleteUserInfo(UserInfo ui);
+}
